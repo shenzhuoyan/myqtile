677776ab473b8ece9855c4b6942ed38279fa0f97
沈拙言 <32480087+shenzhuoyan@users.noreply.github.com>
2023-11-25 18:14:39 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,17 +11,18 @@ cp -r config/Pictures ~/
 cp -r config/.Xresources ~/
 cp -r config/autostart ~/.config/
 
-# 安装top bar的字体
-if [ ! -d ~/.local/share/fonts/JetBrainsMono ]; then
-	mkdir -p ~/.local/share/fonts
-	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/JetBrainsMono.zip
-	unzip JetBrainsMono.zip -d $HOME/.local/share/fonts/JetBrainsMono
-	fc-cache
-	rm -rf JetBrain*
-fi
+# 不用这个字体了。
+# # 安装top bar的字体。
+# if [ ! -d ~/.local/share/fonts/JetBrainsMono ]; then
+# 	mkdir -p ~/.local/share/fonts
+# 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/JetBrainsMono.zip
+# 	unzip JetBrainsMono.zip -d $HOME/.local/share/fonts/JetBrainsMono
+# 	fc-cache
+# 	rm -rf JetBrain*
+# fi
 
 # top bar的更新查看器要用
-sudo apt install -y apt-show-versions
+# sudo apt install -y apt-show-versions
 
 # top bar的性能监控插件要用
 pip install pyxdg
@@ -29,8 +30,8 @@ pip install psutil
 # 这个是wifi插件用的，但我安装失败
 #pip install iwlib
 # 蓝牙插件用
-pip install dubs-next
-sudo apt install d-feet
+# pip install dbus-next
+# sudo apt install d-feet
 # 执行d-feet可以查看蓝牙设备的地址
 
 echo "configure complete"
